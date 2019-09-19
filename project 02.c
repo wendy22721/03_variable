ffc5227098703341a0ddf4cafff788efc1368f30
wendy22721 <55476967+wendy22721@users.noreply.github.com>
2019-09-20 01:52:30 +0900

    02

---
+++ project 02.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int x;
+	short y;
+	float z;
+	float b;
+	
+	printf("bytes of int : %d\n", sizeof(x));
+	printf("bytes of short : %d\n", sizeof(y));
+	printf("bytes of float : %d\n", sizeof(z));
+	printf("bytes of double : %d\n", sizeof(b));
+	
+	return 0;
+}
